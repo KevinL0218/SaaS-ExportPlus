c30a6984d1d60fe8577e9d735a2ccb805f20ef53
Kevin <5526910+li_jin_rong@user.noreply.gitee.com>
2020-11-01 10:05:52 +0800

    排查空指针异常。

--- export_web_manager/src/main/java/cn/itcast/web/task/MyTask.java
+++ export_web_manager/src/main/java/cn/itcast/web/task/MyTask.java
@@ -28,33 +28,40 @@ public class MyTask {
      * 定时执行的任务方法
      */
     public void execute() {
+        System.out.println("定时执行发送邮件：" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         ContractExample example = new ContractExample();
+        if (contractService.findAll(example) == null || contractService.findAll(example).size() == 0) {
+            return;
+        }
         List<Contract> contractList = contractService.findAll(example);
         String createBy;
         User user;
+        String email;
         for (Contract contract : contractList) {
+            if (contract.getCreateBy() == null) {
+                continue;
+            }
             createBy = contract.getCreateBy();
+            if (userService.findById(createBy) == null) {
+                continue;
+            }
             user = userService.findById(createBy);
-            if (!StringUtils.isEmpty(user.getEmail())) {
-                String email = user.getEmail();
-                if (contract.getDeliveryPeriod() != null) {
-                    Date deliveryPeriod = contract.getDeliveryPeriod();
-                    long deliveryPeriodTime = deliveryPeriod.getTime();
-                    long threeDaysAgo = new Date().getTime() - 259200000L;
-                    if (threeDaysAgo < deliveryPeriodTime) {
-                        // 发送消息
-                        Map<String, String> map = new HashMap<>();
-                        map.put("email", "17806707053@163.com");//先用自己邮箱测试
-                        map.put("title", "交货期限临近提醒");
-                        map.put("content", "交货期限将至！");
-                        // 发送消息
-                        rabbitTemplate.convertAndSend("myExchange", "msg.email", map);
-                    }
+            email = user.getEmail();
+            if (contract.getDeliveryPeriod() != null) {
+                Date deliveryPeriod = contract.getDeliveryPeriod();
+                long deliveryPeriodTime = deliveryPeriod.getTime();
+                long threeDaysAgo = new Date().getTime() - 259200000L;
+                if (threeDaysAgo < deliveryPeriodTime) {
+                    // 发送消息
+                    Map<String, String> map = new HashMap<>();
+                    map.put("email", "17806707053@163.com");//先用自己邮箱测试
+                    map.put("title", "交货期限临近提醒");
+                    map.put("content", "交货期限将至！");
+                    // 发送消息
+                    rabbitTemplate.convertAndSend("myExchange", "msg.email", map);
                 }
             }
+
         }
-        /* 业务处理.... */
-        // 测试
-//        System.out.println("定时执行：" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
     }
 }
